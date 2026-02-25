5e455fae557904fdf9ab47f6d0cdae2b0aaf8db6
LeitoM10 <leocuypulga10@gmail.com>
2026-02-25 11:20:38 -0500

    nosotros

---
+++ hope-quito/src/components/Nosotros.jsx
@@ -0,0 +1,13 @@
+// src/components/Nosotros.jsx
+import React from 'react';
+
+const Nosotros = () => {
+  return (
+    <div>
+      <h1>Nosotros</h1>
+      {/* contenido */}
+    </div>
+  );
+};
+
+export default Nosotros; // <-- exportación por defecto
